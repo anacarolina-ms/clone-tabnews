20e288c38e201e681e9e2bb8e9092f94633dc4e1
Ana Carolina Marcelo <100791996+anacarolina-ms@users.noreply.github.com>
2023-07-11 18:46:15 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-function Home(){
-    return <h1>Se você me ama da uma risadinha.</h1>
+function Home() {
+  return <h1>Se você me ama da uma risadinha.</h1>;
 }
-
+function teste() {
+  console.log("teste");
+}
 export default Home;
